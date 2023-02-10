c2d17f9760ed3a66b76f672df212aa787ed1140c
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-02-09 23:22:31 -0300

    variable

---
+++ variable.py
@@ -0,0 +1,7 @@
+name = "George"
+age = 70
+
+print("There once waas a man named ", name)
+print("he was ", age, " years old.")
+print("He really liked the name ", name)
+print("but didn't like being ", age)
