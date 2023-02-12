3ced773bb61bbd99d74525f007c47c7428d1f4fe
Quitumba Ferreira <culyssandercentro@gmail.com>
2023-02-12 14:23:55 -0300

    learn python

--- list.py
+++ list.py
@@ -1,4 +1,30 @@
-friends = ["Raul", "Inock", "Quitumba"]
+friends = ["Raul", "Inock", "Quitumba", "Quinha"]
 
 print(friends)
-print(friends[0])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:3])
+
+
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+# friends.extend(lucky_numbers)
+friends.append("Manuel")
+print(friends)
+
+friends.insert(1, "Bebucho")
+print(friends)
+
+friends.remove("Quitumba")
+print(friends)
+
+# friends.clear()
+# print(friends)
+
+print(friends.pop())
+print(friends.index("Inock"))
+
+print(len(friends))
+
+print(friends.count("Raul"))
+lucky_numbers.reverse()
+print(lucky_numbers)
